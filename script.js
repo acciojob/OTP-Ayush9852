f496c785601cb6a7aa673b9e6299c03675b481aa
Ayush9852 <71625438+Ayush9852@users.noreply.github.com>
2024-03-04 16:51:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const inputs = document.querySelectorAll('.code');
+
+  // Add event listener to each input
+  inputs.forEach((input, index) => {
+    input.addEventListener('input', function(event) {
+      const value = event.target.value;
+      if (value.length === 1 && index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    });
+
+    // Add event listener for backspace key press
+    input.addEventListener('keydown', function(event) {
+      if (event.key === 'Backspace' && index > 0) {
+        if (input.value === '') {
+          inputs[index - 1].focus();
+        }
+      }
+    });
+  });
+});
